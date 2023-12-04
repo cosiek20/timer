1e3af0c7402a9f4bb96ef646564e48c16c82cba8
Filip Stasiak <filipstasiakbarlinek@gmail.com>
2023-12-04 11:42:55 +0100

    finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Timer from './components/Timer/Timer';
+import { useEffect } from 'react';
+import Button from './components/Button/Button';
+import Container from './components/Container/Container';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [time, setTime] = useState(0);
+	const [timer, setTimer] = useState(null);
+
+	useEffect(() => {
+    return () => {
+      if(timer) clearInterval(timer);
+   };
+
+	}, [timer]);
+
+	const handleStart = () => {
+    setTimer(setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1))
+	};
+
+	const handleStop = () => {
+		setTimer(null);
+	};
+
+	const handleReset = () => {
+		setTime(0);
+		setTimer(null);
+	};
+
+	return (
+    <Container>
+      <Timer time={time}></Timer>
+			<Button onClick={handleStart}>Start</Button>
+			<Button onClick={handleStop}>Stop</Button>
+			<Button onClick={handleReset}>Reset</Button>
+    </Container>
+	);
+};
 
 export default App;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,30 @@
+import styles from './Timer.module.scss'
+
+const Timer = ({time}) => {
+
+  function formatTime(s) {
+
+    // Pad to 2 or 3 digits, default is 2
+    function pad(n, z) {
+      z = z || 2;
+      return ('00' + n).slice(-z);
+    }
+  
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+  
+    return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
+  }
+
+  return(
+    <div className={styles.timer}>
+      {formatTime(time)}
+    </div>
+  )
+}
+
+export default Timer;
